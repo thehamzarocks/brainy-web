b4d8e45ca81f5fde52a45ba2995835b8e233efe1
thehamzarocks <thehamzarocks@gmail.com>
2020-07-22 20:56:25 +0530

    adding task and info tabs

--- brainyweb/src/ScratchFile.js
+++ brainyweb/src/ScratchFile.js
@@ -3,6 +3,11 @@ import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Box from "@material-ui/core/Box";
+import PropTypes from "prop-types";
 
 const useStyles = makeStyles(() => ({
   fileHeader: {
@@ -17,13 +22,56 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
 function ScatchFile() {
   const classes = useStyles();
 
   const [value, setValue] = React.useState("");
+  const [taskValue, setTaskValue] = React.useState("");
+  const [infoValue, setInfoValue] = React.useState("");
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleTaskChange = (event, newValue) => {
+    setTaskValue(event.target.value);
+  };
+
+  const handleInfoChange = (event, newValue) => {
+    setInfoValue(event.target.value);
   };
 
   return (
@@ -36,27 +84,42 @@ function ScatchFile() {
           <Typography variant="h6">Tags: scratchfile</Typography>
         </div>
         <div>
-          <Button color="default">
-            Actions
-          </Button>
+          <Button color="default">Actions</Button>
         </div>
         <div>
-          <Button color="primary">
-            Save
-          </Button>
+          <Button color="primary">Save</Button>
         </div>
       </div>
       <form className={classes.root} noValidate autoComplete="off">
         <div>
+          <AppBar position="static">
+            <Tabs value={value} onChange={handleChange} aria-label="file tabs">
+              <Tab label="Tasks" {...a11yProps(0)} />
+              <Tab label="Information" {...a11yProps(1)} />
+            </Tabs>
+          </AppBar>
+          <TabPanel value={value} index={0}>
+            <TextField
+              spellCheck="false"
+              fullWidth="true"
+              rows="30"
+              id="standard-multiline-flexible"
+              multiline
+              value={taskValue}
+              onChange={handleTaskChange}
+            />
+          </TabPanel>
+          <TabPanel value={value} index={1}>
           <TextField
-          spellCheck="false"
-            fullWidth="true"
-            rows="30"
-            id="standard-multiline-flexible"
-            multiline
-            value={value}
-            onChange={handleChange}
-          />
+              spellCheck="false"
+              fullWidth="true"
+              rows="30"
+              id="standard-multiline-flexible"
+              multiline
+              value={infoValue}
+              onChange={handleInfoChange}
+            />
+          </TabPanel>
         </div>
       </form>
     </React.Fragment>
